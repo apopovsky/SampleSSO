02bf43a81f1953bb006dfebd704e55ee880b627d
Ariel Popovsky <apopovsky@itelligent.com.ar>
2014-06-04 17:04:19 -0300

    Initial Commit

---
+++ IdProvider/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using SAML2;
+using SAML2.Schema.Core;
+using SAML2.Schema.Protocol;
+using SAML2.Utils;
+
+namespace IdProvider.Controllers
+{
+    [HandleError(View = "Error")]
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [ValidateInput(false)]
+        public ActionResult Index(Saml20AuthnRequest samlRequest, string relayState)
+        {
+            var response = new Response();
+            var status = new Status();
+            response.Status = status;
+            status.StatusCode = new StatusCode
+            {
+                Value = Saml20Constants.StatusCodes.Success
+            };
+            var assertion = new Assertion {Id = "ass_1", IssueInstant = DateTime.UtcNow, Version = "1.0"};
+            response.Items = new object[] {assertion};
+            response.InResponseTo = "message_1";
+
+            var nameId = new NameId {Value = "123"};
+            response.Issuer = nameId;
+
+            var model = new SAMLResponseViewModel { SAMLResponse = Serialization.SerializeToXmlString(response), RelayState = relayState };
+            return View(model);
+        }
+
+    }
+
+    public class SAMLResponseViewModel
+    {
+        public string SAMLResponse { get; set; }
+        public string RelayState { get; set; }
+    }
+}
---
+++ ServiceProvider/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using SAML2;
+using SAML2.Protocol;
+using SAML2.Schema.Core;
+using SAML2.Schema.Protocol;
+using SAML2.Utils;
+
+namespace ServiceProvider.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var request = new Saml20AuthnRequest();
+            request.Id = "request_1";
+            request.Request.Version = "1.0";
+            var nameIdPolicy = new NameIdPolicy {Format = Saml20Constants.NameIdentifierFormats.Email};
+            request.Request.NameIdPolicy = nameIdPolicy;
+            request.Issuer = "http://localhost:15881/";
+
+            var model = new SamlRequestViewModel { SAMLRequest = request.GetXml().InnerXml, RelayState = Session.SessionID };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(Response samlResponse, string relayState)
+        {
+            /*var handler = new Saml20SignonHandler();
+            handler.ProcessRequest(System.Web.HttpContext.Current);*/
+            ViewBag.Message = "Bienvenido " + User.Identity.Name;
+            return View();
+        }
+
+    }
+
+    public class SamlRequestViewModel
+    {
+        public string SAMLRequest { get; set; }
+        public string RelayState { get; set; }
+    }
+}
