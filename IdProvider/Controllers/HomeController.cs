efdd53739b0cb7b27e3ae19279051871f86be81f
Ariel Popovsky <apopovsky@itelligent.com.ar>
2014-06-04 19:15:50 -0300

    Added file certificates and signing test.

--- IdProvider/Controllers/HomeController.cs
+++ IdProvider/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 using SAML2;
 using SAML2.Schema.Core;
 using SAML2.Schema.Protocol;
+using SAML2.Schema.XmlDSig;
 using SAML2.Utils;
 
 namespace IdProvider.Controllers
@@ -22,7 +23,42 @@ namespace IdProvider.Controllers
             {
                 Value = Saml20Constants.StatusCodes.Success
             };
-            var assertion = new Assertion {Id = "ass_1", IssueInstant = DateTime.UtcNow, Version = "1.0"};
+            var issuer = new NameId
+            {
+                Format = Saml20Constants.NameIdentifierFormats.Transient,
+                Value = "http://localhost:35513/"
+            };
+            var signature = new Signature
+            {
+                SignedInfo = new SignedInfo
+                {
+                    CanonicalizationMethod =
+                        new CanonicalizationMethod {Algorithm = "http://www.w3.org/2001/10/xml-exc-c14n#"},
+                    SignatureMethod = new SignatureMethod
+                    {
+                        Algorithm = "http: //www.w3.org/2000/09/xmldsig#rsa-sha1"
+                    },
+                    Reference = new[]
+                    {
+                        new Reference
+                        {
+                            DigestMethod = new DigestMethod
+                            {
+                                Algorithm = "http://www.w3.org/2000/09/xmldsig#sha1"
+                            },
+                            //Transforms = new []{new Transform{Algorithm = }}
+                        }
+                    }
+                }
+            };
+            var assertion = new Assertion
+            {
+                Id = "ass_1",
+                IssueInstant = DateTime.UtcNow,
+                Version = "1.0",
+                Issuer = issuer,
+                Signature = signature
+            };
             response.Items = new object[] {assertion};
             response.InResponseTo = "message_1";
 
--- ServiceProvider/Controllers/HomeController.cs
+++ ServiceProvider/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml;
@@ -9,12 +15,21 @@ using SAML2.Protocol;
 using SAML2.Schema.Core;
 using SAML2.Schema.Protocol;
 using SAML2.Utils;
+using KeyInfo = SAML2.Schema.XmlDSig.KeyInfo;
+using Reference = SAML2.Schema.XmlDSig.Reference;
 
 namespace ServiceProvider.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            var requestXml = GetRequestXml();
+            var model = new SamlRequestViewModel {SAMLRequest = requestXml.InnerXml, RelayState = Session.SessionID};
+            return View(model);
+        }
+
+        private static XmlDocument GetRequestXml()
         {
             var request = new Saml20AuthnRequest();
             request.Id = "request_1";
@@ -23,8 +38,8 @@ namespace ServiceProvider.Controllers
             request.Request.NameIdPolicy = nameIdPolicy;
             request.Issuer = "http://localhost:15881/";
 
-            var model = new SamlRequestViewModel { SAMLRequest = request.GetXml().InnerXml, RelayState = Session.SessionID };
-            return View(model);
+            var requestXml = request.GetXml();
+            return requestXml;
         }
 
         [HttpPost]
@@ -37,6 +52,39 @@ namespace ServiceProvider.Controllers
             return View();
         }
 
+        public ActionResult Test()
+        {
+            var requestXml = GetRequestXml();
+            var sig = new SignedXml(requestXml);
+            var key = new RSACryptoServiceProvider();
+
+            // Add the key to the SignedXml xmlDocument.
+            sig.SigningKey = key;
+
+            // Create a reference to be signed.
+            var reference = new System.Security.Cryptography.Xml.Reference {Uri = ""};
+
+            // Add an enveloped transformation to the reference.
+            var env = new XmlDsigEnvelopedSignatureTransform();
+            reference.AddTransform(env);
+
+            // Add the reference to the SignedXml object.
+            sig.AddReference(reference);
+
+            var path =Path.Combine(new DirectoryInfo(HttpContext.Server.MapPath(@"~\")).Parent.FullName, "sign.crt");
+            var cert = X509Certificate.CreateFromCertFile(path);
+            // Add an RSAKeyValue KeyInfo (optional; helps recipient find key to validate).
+            var keyInfo = new System.Security.Cryptography.Xml.KeyInfo();
+            keyInfo.AddClause(new KeyInfoX509Data(cert));
+            sig.KeyInfo = keyInfo;
+
+            // Compute the signature.
+
+            sig.ComputeSignature();
+            // Get the XML representation of the signature and save
+            // it to an XmlElement object.
+            return Content("Signature = " + sig.GetXml().OuterXml);
+        }
     }
 
     public class SamlRequestViewModel
